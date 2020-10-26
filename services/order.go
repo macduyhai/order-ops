2e5af92bf0ef3fb6dbb3a33d22f2888244920422
haimd <haimd@beetsoft.com.vn>
2020-10-27 00:51:59 +0700

    .

--- services/order.go
+++ services/order.go
@@ -151,7 +151,7 @@ func (service *orderServiceImpl) mapperDtossToModelItemAddLable(input dtos.Item)
 //  mapperDtossToModelItemAddLable(input dtos.Item) models.Item
 func (service *orderServiceImpl) AddLabelsToItems(request dtos.AddLabelRequest) (*dtos.AddorderResponse, error) {
 	res := dtos.AddorderResponse{}
-
+	log.Println(request)
 	for _, item := range request.Items {
 		if item.OrderNumber == "" {
 			item.OrderNumber = request.OrderNumber
