d9e81c073e2ab564597acc9385e2dd0ffcc86676
haimd <haimd@beetsoft.com.vn>
2020-12-01 11:53:13 +0700

    Loc ki tu space trong state

--- services/order.go
+++ services/order.go
@@ -73,7 +73,7 @@ func (service *orderServiceImpl) mapperDtossToModelOrder(input dtos.Order) model
 	input.BranchSell = service.Filtercharacter(input.BranchSell)
 	input.TypeProduct = service.Filtercharacter(input.TypeProduct)
 	input.Seller = service.Filtercharacter(input.Seller)
-	imput.State = service.Filtercharacter(imput.State)
+	input.State = service.Filtercharacter(input.State)
 	t_n := time.Now().Add(+7 * time.Hour)
 
 	return models.Order{
@@ -99,7 +99,7 @@ func (service *orderServiceImpl) mapperDtossToModelOrderFull(input dtos.OrderFul
 	// input.Country = service.Filtercharacter(input.Country)
 	input.BranchSell = service.Filtercharacter(input.BranchSell)
 	input.Seller = service.Filtercharacter(input.Seller)
-	imput.State = service.Filtercharacter(imput.State)
+	input.State = service.Filtercharacter(input.State)
 	t_n := time.Now().Add(+7 * time.Hour)
 
 	return models.Order{
